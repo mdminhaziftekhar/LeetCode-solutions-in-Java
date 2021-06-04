5d6e09c957a7c8764aa785cea0b1ec865254fcfc
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-06-04 20:32:32 +0600

    1480. Running Sum of 1d Array

---
+++ runningSum1480.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] a = new int[nums.length];
+        
+        int sum = 0;
+        
+        for(int i = 0; i<nums.length; i++){
+            sum = sum + nums[i];
+            a[i] = sum;
+        }
+        
+        return a;
+    }
+}
