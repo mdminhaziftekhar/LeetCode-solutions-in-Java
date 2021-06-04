45bd1563fdfb2285570cb584bc189968140b0231
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-06-04 20:25:57 +0600

    771. Jewels and Stones

---
+++ jewelsAndStones771.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        
+        int sum = 0;
+        
+        for(int i = 0; i<jewels.length(); i++){
+            
+            for(int j = 0; j<stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j)) sum++;    
+            }
+        }
+        
+        return sum;
+    }
+}
